d9fcb07f24777d43340555eb8b8a9d630c60011f
shAdowplusing <1290232854@qq.com>
2023-02-07 23:28:31 +0800

    just register the EventBus singleton in app_get_it.dart, try to use it next time.

--- lib/state_machine/get_it/app_get_it.dart
+++ lib/state_machine/get_it/app_get_it.dart
@@ -1,3 +1,4 @@
+import 'package:event_bus/event_bus.dart';
 import 'package:get_it/get_it.dart';
 import 'package:writing_writer/server/file/IOBase.dart';
 
@@ -7,4 +8,7 @@ GetIt appGetIt = GetIt.instance;
 Future<void> setupAppGetIt({bool test = false}) async {
   /// tool one -- IOBase : file IO tool class register
   appGetIt.registerSingleton<IOBase>(IOBase());
+
+  /// tool two -- EventBus : EventBus Stream tool class register
+  appGetIt.registerSingleton<EventBus>(EventBus());
 }
