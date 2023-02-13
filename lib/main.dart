76a3b7945476712c3fd40941e12bb28ac1b31037
shAdowplusing <1290232854@qq.com>
2023-02-13 17:56:40 +0800

    add instance.ensureInitialized in void main() entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:writing_writer/state_machine/get_it/app_get_it.dart';
 
-/// pc 端
-import 'entry/pc/pc_main.dart';
-void main() {
-  setupAppGetIt();
-  runApp(PcApp());
-}
-
-// /// mobile 端
-// import 'entry/mobile/mobile_main.dart';
+// /// pc 端
+// import 'entry/pc/pc_main.dart';
 // void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
 //   setupAppGetIt();
-//   runApp(MobileApp());
+//   runApp(PcApp());
 // }
+
+/// mobile 端
+import 'entry/mobile/mobile_main.dart';
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupAppGetIt();
+  runApp(MobileApp());
+}
