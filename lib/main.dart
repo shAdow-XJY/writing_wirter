42220e986216d3eca2689531ab18a19996fec8f9
shAdowplusing <1290232854@qq.com>
2023-05-04 19:41:08 +0800

    change main.dart to mobile for codemagic test

--- lib/main.dart
+++ lib/main.dart
@@ -20,28 +20,28 @@ import 'package:writing_writer/state_machine/get_it/app_get_it.dart';
 //   });
 // }
 
-// /// mobile 端
-// import 'entry/mobile/mobile_main.dart';
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await setupAppGetIt();
-//   runApp(MobileApp());
-// }
-
-/// mobile 端 在desktop端 运行，debug 方便用
+/// mobile 端
 import 'entry/mobile/mobile_main.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupAppGetIt();
   runApp(MobileApp());
-  doWhenWindowReady(() {
-    final win = appWindow;
-    const initialSize = Size(400, 750);
-    win.minSize = initialSize;
-    win.size = initialSize;
-    win.alignment = Alignment.centerLeft;
-    win.title = "Writing Writer";
-    win.show();
-  });
 }
+
+// /// mobile 端 在desktop端 运行，debug 方便用
+// import 'entry/mobile/mobile_main.dart';
+// import 'package:bitsdojo_window/bitsdojo_window.dart';
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await setupAppGetIt();
+//   runApp(MobileApp());
+//   doWhenWindowReady(() {
+//     final win = appWindow;
+//     const initialSize = Size(400, 750);
+//     win.minSize = initialSize;
+//     win.size = initialSize;
+//     win.alignment = Alignment.centerLeft;
+//     win.title = "Writing Writer";
+//     win.show();
+//   });
+// }
